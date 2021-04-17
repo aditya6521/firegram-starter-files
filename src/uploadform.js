29b5cda24e21fdbbf793bc338d65c32ea0329d7f
aditya6521 <adityasinghr15@gmail.com>
2021-04-18 01:44:43 +0530

    refractor hooks

--- src/uploadform.js
+++ src/uploadform.js
@@ -3,13 +3,23 @@ import React, { useState } from "react";
 const Uploadform = ()=> {
 
     const [file,setFile] = useState(null);
+    const [error,setError]= useState(null);
+
+    const types=["image/png","image/jpeg"];
 
 
     function changeHandler(e){
         let selected=e.target.files[0];
+        console.log(selected);
         
-        if(selected){
+        if(selected && types.includes(selected.type)){
             setFile(selected);
+            setError("");
+        } 
+        else {
+            setFile(null);
+            console.log("only jpeg and png files");
+            setError("only jpeg and png files");
         }
     };
 
@@ -17,6 +27,10 @@ const Uploadform = ()=> {
 
         <form>
             <input type="file" onChange={changeHandler}/>
+            <div className="output">
+                {error && <div className="error"> {error}</div>}
+                {file && <div>{file.name}</div>}
+            </div>
         </form>
 
     )
