38c18dce12613845b25ece2b1b2cc2509a0728cb
aditya6521 <adityasinghr15@gmail.com>
2021-04-17 13:10:42 +0530

    added firebase config SDK

---
+++ src/firebase_config.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+ var firebaseConfig = {
+    apiKey: "AIzaSyDI0ZPQZYHAtRsJ7bEZt79GdOfbYKUAYRU",
+    authDomain: "photogen-ba786.firebaseapp.com",
+    projectId: "photogen-ba786",
+    storageBucket: "photogen-ba786.appspot.com",
+    messagingSenderId: "125727714625",
+    appId: "1:125727714625:web:db15d3dab7b20ae05305c0"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const projectStorage=firebase.storage();
+  const projectFirestore=firebase.firestore();
+
+  export { projectStorage , projectFirestore};
